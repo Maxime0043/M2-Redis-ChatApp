ea5d512b011ce1cf810b55321b125d9a7e0b3e40
Maxime0043 <57800452+Maxime0043@users.noreply.github.com>
2023-01-06 01:05:04 +0100

    [Chat Websocket] Middleware: verification of the owner of the room

--- chat-websocket/app/middlewares/index.js
+++ chat-websocket/app/middlewares/index.js
@@ -3,7 +3,8 @@ const { buildError } = require("../utils/errors.func");
 
 module.exports = (socket, redis) => {
   socket.use(async ([event, ...args], next) => {
-    const checkIfRoomExists = ["join-room"];
+    const checkIfRoomExists = ["join-room", "delete-room"];
+    const checkIfUserIsOwner = ["delete-room"];
 
     // We add the name of the triggering event in the socket request
     socket.request.eventTriggered = event;
@@ -26,6 +27,19 @@ module.exports = (socket, redis) => {
       const roomExistsError = buildError("room-not-existing", event);
 
       if (!roomExists) return next(roomExistsError);
+
+      // Check if the user is the owner of the room
+      if (checkIfUserIsOwner.includes(event)) {
+        // Verify if the variable "idUser" is valid
+        if (!data?.idUser || data?.idUser?.length == 0)
+          return next(invalidDataError);
+
+        const { idUser } = data;
+        const owner = room.owner;
+        const isOwnerError = buildError("user-not-owner", event);
+
+        if (idUser != owner) return next(isOwnerError);
+      }
     }
 
     next();
