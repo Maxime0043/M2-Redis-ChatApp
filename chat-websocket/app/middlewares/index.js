a2e92bd332a709c017b8f0a85cfccde047375905
Maxime0043 <57800452+Maxime0043@users.noreply.github.com>
2023-01-06 01:16:28 +0100

    [Chat Websocket] Middleware: verification of the member of the room + verification of the length of the new message

--- chat-websocket/app/middlewares/index.js
+++ chat-websocket/app/middlewares/index.js
@@ -1,10 +1,13 @@
 const { getRoom } = require("../utils/rooms.func");
+const { getMessage } = require("../utils/messages.func");
 const { buildError } = require("../utils/errors.func");
 
 module.exports = (socket, redis) => {
   socket.use(async ([event, ...args], next) => {
-    const checkIfRoomExists = ["join-room", "delete-room"];
+    const checkIfRoomExists = ["join-room", "delete-room", "new-message"];
     const checkIfUserIsOwner = ["delete-room"];
+    const checkIfUserIsMember = ["new-message"];
+    const checkMessageLength = ["new-message"];
 
     // We add the name of the triggering event in the socket request
     socket.request.eventTriggered = event;
@@ -39,6 +42,32 @@ module.exports = (socket, redis) => {
         const isOwnerError = buildError("user-not-owner", event);
 
         if (idUser != owner) return next(isOwnerError);
+      }
+
+      // Check if the user is a member of the room
+      if (checkIfUserIsMember.includes(event)) {
+        // Verify if the variable "idUser" is valid
+        if (!data?.idUser || data?.idUser?.length == 0)
+          return next(invalidDataError);
+
+        const { idUser } = data;
+        const isMember = await redis.SISMEMBER(`users:${idUser}:rooms`, idRoom);
+        const isMemberError = buildError("user-not-member", event);
+
+        if (!isMember) return next(isMemberError);
+
+        // Checks if the message length is valid
+        if (checkMessageLength.includes(event)) {
+          // Verify if the variable "message" is valid
+          if (!data?.message || data?.message?.length == 0)
+            return next(invalidDataError);
+
+          const { message } = data;
+          const isMessageLengthError = buildError("message-too-long", event);
+
+          // If the message is longer than 2000 characters
+          if (message.length > 2000) return next(isMessageLengthError);
+        }
       }
     }
 
