651cb974cbe8df5953c3ce67710660fbae1211c2
Maxime0043 <57800452+Maxime0043@users.noreply.github.com>
2023-01-06 01:22:39 +0100

    [Chat Websocket] Implementation of listening on the "delete-message" event

--- chat-websocket/app/controllers/messages.js
+++ chat-websocket/app/controllers/messages.js
@@ -1,3 +1,5 @@
+const { getMessage } = require("../utils/messages.func");
+
 module.exports = async (io, socket, redis) => {
   socket.on("new-message", async (data) => {
     const { idUser, idRoom, message } = data;
@@ -19,5 +21,23 @@ module.exports = async (io, socket, redis) => {
     io.to(idRoom).emit("new-message", { message: messageData });
 
     console.log(`[NEW MESSAGE] ${idRoom} : ${JSON.stringify(messageData)}`);
+  });
+
+  socket.on("delete-message", async (data) => {
+    const { idRoom, idMessage } = data;
+
+    // Retrieve the message to delete
+    const message = await getMessage(redis, idRoom, idMessage);
+
+    // If the message has not been found
+    if (!message) return;
+
+    // Delete the message from the room
+    await redis.SREM(`rooms:${idRoom}:messages`, JSON.stringify(message));
+
+    // We indicate to the users of the room the message to delete
+    io.to(idRoom).emit("delete-message", { idMessage });
+
+    console.log(`[DELETE MESSAGE] ${idRoom} : ${idMessage}`);
   });
 };
