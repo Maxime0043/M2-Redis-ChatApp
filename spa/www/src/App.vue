fb37c9eff2b398ecf3bc9ac5a7f4d72437bc6823
Micaël <micaeldupuis@live.fr>
2023-01-08 21:17:40 +0100

    Add defaut vue3 app

---
+++ spa/www/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import { RouterView } from 'vue-router'
+</script>
+<template>
+  <RouterView v-slot="{ Component }">
+    <template v-if="Component">
+      <Suspense>
+        <!-- main content -->
+        <component :is="Component"></component>
+
+        <!-- loading state -->
+        <template #fallback> Loading... </template>
+      </Suspense>
+    </template>
+  </RouterView>
+</template>
